a44ba66729b6c1e616c3c9c12c26ea0d46ba3ac7
Mehmet <mehmetk6158@gmail.com>
2021-11-30 19:22:56 -0500

    add show action

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,4 +3,11 @@ class TeamsController < ApplicationController
     @teams = Team.all
     render 'index.html.erb'
   end
+  
+  def show
+
+    @team = Team.find_by(id: params[:id])
+    render 'show.html.erb'
+    
+  end
 end

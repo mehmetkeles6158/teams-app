757606b25a6ca8c5585137df6fdbb77f00ec2e00
Mehmet <mehmetk6158@gmail.com>
2021-11-30 20:14:37 -0500

    create action

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,11 +3,27 @@ class TeamsController < ApplicationController
     @teams = Team.all
     render 'index.html.erb'
   end
-  
+
   def show
 
     @team = Team.find_by(id: params[:id])
     render 'show.html.erb'
     
+  end
+
+  def create
+    @team = Team.new(
+      name: params[:name],
+      stadium: params[:stadium],
+      value: params[:value]
+    )
+    @team.save
+    # render 'create.html.erb'
+
+    redirect_to "/teams/#{@team.id}"
+  end
+
+  def new
+    render 'new.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/teams" =>"teams#index"
+  get "/teams/new" =>"teams#new"
   get "/teams/:id" =>"teams#show"
+  post "/teams" =>"teams#create"
+  
 end
